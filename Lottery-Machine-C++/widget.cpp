fc1be846c5564160f5027b11c4ec69818d01793a
Aviation Engineer <www.yebeichen@qq.com>
2022-12-03 13:24:59 +0800

    Faster Lottery-Machine.

---
+++ Lottery-Machine-C++/widget.cpp
@@ -0,0 +1,34 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+#include<QRandomGenerator>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    setWindowTitle("Lottery Machine");
+    QIntValidator *validator = new QIntValidator(0, 100, this);
+    ui->mine->setValidator(validator);
+    ui->maxe->setValidator(validator);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_quib_clicked()
+{
+    close();
+}
+
+void Widget::on_genb_clicked()
+{
+    int mi=ui->mine->text().toInt();
+    int ma=ui->maxe->text().toInt();
+    int r=QRandomGenerator::global()->bounded(mi,ma)-49;
+    char rc=r + '0';
+    QMessageBox::information(this, tr("Generate"),tr("Number %1.").arg(rc+1),QMessageBox::Ok,QMessageBox::NoButton);
+}
