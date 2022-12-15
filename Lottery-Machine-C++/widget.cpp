ab056ddf6ead0269daf3d477aefbf4b381050705
Aviation Engineer <www.yebeichen@qq.com>
2022-12-15 20:03:44 +0800

    Repair.

--- Lottery-Machine-C++/widget.cpp
+++ Lottery-Machine-C++/widget.cpp
@@ -52,7 +52,7 @@ void Widget::on_gas_clicked()
     int mi=ui->mine->text().toInt();
     int ma=ui->maxe->text().toInt();
     int n=ui->noe->text().toInt();
-    int r=generate(mi,ma,n)+48;
+    int r=generate(mi,ma,n)+49;
     QString sr=QString("%1").arg(r);
     speech->say(sr);
     ui->gas->setEnabled(false);
