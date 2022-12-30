fae347a32e686bbd7128e803251843357aada0d5
Aviation Engineer <www.yebeichen@qq.com>
2022-12-30 11:31:11 +0800

    Lottery-Machine-C#(.NET)

---
+++ Lottery-Machine-C#/Lottery-Machine/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lottery_Machine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mins = mint.Text.ToString();
+            string maxs = maxt.Text.ToString();
+            int mini, maxi;
+            bool minb = int.TryParse(mins,out mini);
+            bool maxb = int.TryParse(maxs, out maxi);
+            if (minb==true&&maxb==true)
+            {
+                mini=int.Parse(mins);
+                maxi=int.Parse(maxs);
+                Random rd = new Random();
+                int r = rd.Next(mini, maxi + 1);
+                string rs = r.ToString();
+                MessageBox.Show($"Number {rs}.", "Generate", 0, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct numbers.");
+            }
+        }
+    }
+}
