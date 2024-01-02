421c207fd6097d01d3841528a741118bad1ed467
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2024-01-02 20:01:47 +0800

    Update ASA.py

--- ASA.py
+++ ASA.py
@@ -12,5 +12,5 @@ angle3 = 180 - angle1 - angle2
 side1 = side * math.sin(math.radians(angle1)) / math.sin(math.radians(angle3))
 side2 = side * math.sin(math.radians(angle2)) / math.sin(math.radians(angle3))
 # 输出结果
-print("第三边的长度为：", side1)
-print("第四边的长度为：", side2)
+print("第二边的长度为：", side1)
+print("第三边的长度为：", side2)
